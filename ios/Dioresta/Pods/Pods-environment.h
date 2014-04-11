0e2f700aa8419e7ab8f9d04760ba79dd70c6fd5b
Brian Gilham <me@briangilham.com>
2014-04-11 19:54:19 -0400

    Trying SocketRocket

--- ios/Dioresta/Pods/Pods-environment.h
+++ ios/Dioresta/Pods/Pods-environment.h
@@ -12,3 +12,9 @@
 #define COCOAPODS_VERSION_MINOR_CocoaAsyncSocket 3
 #define COCOAPODS_VERSION_PATCH_CocoaAsyncSocket 4
 
+// SocketRocket
+#define COCOAPODS_POD_AVAILABLE_SocketRocket
+#define COCOAPODS_VERSION_MAJOR_SocketRocket 0
+#define COCOAPODS_VERSION_MINOR_SocketRocket 3
+#define COCOAPODS_VERSION_PATCH_SocketRocket 1
+
