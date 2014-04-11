252291f273dbeef7532c94792064c4b923e978ac
Brian Gilham <me@briangilham.com>
2014-04-11 18:57:12 -0400

    Added base iOS project

---
+++ ios/Dioresta/Dioresta/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Dioresta
+//
+//  Created by Brian Gilham on 2014-04-11.
+//  Copyright (c) 2014 Space Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DIOAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DIOAppDelegate class]));
+    }
+}
